9b344b0ebd69b912fbeb48ac86ff3b4dd08693e3
Anirban Ghosh <anirbanghosh060@gmail.com>
2025-08-27 18:36:56 +0530

    Add CORS middleware, update requirements, and configure Vercel deployment

--- main_api.py
+++ main_api.py
@@ -1,14 +1,28 @@
 from fastapi import FastAPI
 from supabase import create_client, Client
 import os
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+origins =[
+    "http://localhost:3000",
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 SUPABASE_URL = os.getenv('SUPABASE_URL')
 SUPABASE_KEY = os.getenv('SUPABASE_KEY')
 TABLE_NAME = 'indian_college_news'
 
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
+
+@app.get("/")
+def root():
+    return {"message": "Indian College News API is running!"}
 
 @app.get("/news")
 def get_news():
--- mongo.py
+++ mongo.py
@@ -2,7 +2,7 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
 
-uri = "mongodb+srv://dsuper03dev:swamohan@cluster0.m6yvywi.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = ""
 
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
